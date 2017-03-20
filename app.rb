8ef5cb4234f8d34af3f1344d65433acfc48a00f1
Flora Harvey <fharvey555@gmail.com>
2017-03-20 17:45:21 +0000

    Links create listings with model

--- app.rb
+++ app.rb
@@ -1,14 +1,17 @@
 ENV['RACK_ENV'] ||= 'development'
 
 require 'sinatra/base'
+require './app/models/datamapper_setup'
 
 class Air_bnb < Sinatra::Base
 
   get '/' do
+    @spaces = Space.all
     erb :index
   end
 
   post '/listings/new' do
+    Space.create(name: params[:name], description: params[:description], price: params[:price])
     redirect '/'
   end
 
