a9d15d181307a792da75ce30df7bd6e01d6d5cb3
allthatilk <imogenkutz@hotmail.com>
2017-03-20 17:57:42 +0000

    Connected database url for heroku

--- app/models/datamapper_setup.rb
+++ app/models/datamapper_setup.rb
@@ -3,6 +3,6 @@ require 'dm-postgres-adapter'
 require_relative './space'
 
 DataMapper::Logger.new($stdout, :debug)
-DataMapper.setup(:default, "postgres://localhost/bnb_#{ENV["RACK_ENV"]}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bnb_#{ENV["RACK_ENV"]}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
