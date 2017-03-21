a1472190b9a9d247bf3e6e3f528d59bc918270c7
Flora Harvey <fharvey555@gmail.com>
2017-03-21 12:27:52 +0000

    Add method to space to check if available tonight

--- app/models/space.rb
+++ app/models/space.rb
@@ -7,4 +7,10 @@ class Space
   property :description, String
   property :price, Integer
 
+  has n, :bookings
+
+  def available_tonight?
+    Booking.all(space_id: id, date: Date.today).empty?
+  end
+
 end
--- spec/space_spec.rb
+++ spec/space_spec.rb
@@ -1,6 +1,20 @@
 describe Space do
+
   it "can create a new space" do
     Space.create(name: "Two-bedroom", description: "Spacious flat in Kingston", price: 70)
     expect(Space.all.count).to eq 1
   end
+
+  it "knows if it's available tonight" do
+    my_space = Space.create(name: "Two-bedroom", description: "Spacious flat in Kingston", price: 70)
+    expect(my_space.available_tonight?).to be true
+  end
+
+  it "knows if it's unavailable tonight" do
+    my_space = Space.create(name: "Two-bedroom", description: "Spacious flat in Kingston", price: 70)
+    booking = Booking.create(date: Date.today, space_id: 1)
+    expect(my_space.available_tonight?).to be false
+  end
+
+
 end
