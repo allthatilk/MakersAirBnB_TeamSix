86d8042ff7b877dab9991b319b1a619b44dca1e9
Alice Armstrong <tioma001@hotmail.com>
2017-03-21 12:35:38 +0000

    Implementing password toggle button

---
+++ public/toggle.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+	function togglePasswordFieldClicked() {
+
+	var passwordField = document.getElementById('passwordField');
+	var value = passwordField.value;
+
+	if(passwordField.type == 'password') {
+		passwordField.type = 'text';
+	}
+	else {
+		passwordField.type = 'password';
+	}
+
+	passwordField.value = value;
+
+}
+}
