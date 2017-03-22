846e2fdf470ca94b0196a2d852d117c9ce091b22
allthatilk <imogenkutz@hotmail.com>
2017-03-22 13:11:23 +0000

    WIP added is_approved property to bookings and added method for approving bookings

--- spec/space_spec.rb
+++ spec/space_spec.rb
@@ -1,35 +1,31 @@
 describe Space do
-
+  let(:my_space) { Space.create(name: "Two-bedroom", description: "Spacious flat in Kingston", price: 70) }
+  let(:booking) { Booking.create(date: Date.today, space_id: 1) }
   it "can create a new space" do
-    Space.create(name: "Two-bedroom", description: "Spacious flat in Kingston", price: 70)
+    my_space
     expect(Space.all.count).to eq 1
   end
 
   it "knows if it's available tonight" do
-    my_space = Space.create(name: "Two-bedroom", description: "Spacious flat in Kingston", price: 70)
     expect(my_space.available_tonight?).to be true
   end
 
   it "knows if it's unavailable tonight" do
-    my_space = Space.create(name: "Two-bedroom", description: "Spacious flat in Kingston", price: 70)
-    booking = Booking.create(date: Date.today, space_id: 1)
+    booking
     expect(my_space.available_tonight?).to be false
   end
 
   it "knows if it's available on a given date" do
-    my_space = Space.create(name: "Two-bedroom", description: "Spacious flat in Kingston", price: 70)
     expect(my_space.available_on_date?(Date.today)).to be true
   end
 
   it "knows if it's unavailable on a given date" do
-    my_space = Space.create(name: "Two-bedroom", description: "Spacious flat in Kingston", price: 70)
-    booking = Booking.create(date: Date.today, space_id: 1)
+    booking
     expect(my_space.available_on_date?(Date.today)).to be false
   end
 
   it "shows unavailable dates for a space" do
-    my_space = Space.create(name: "Two-bedroom", description: "Spacious flat in Kingston", price: 70)
-    booking = Booking.create(date: Date.today, space_id: 1)
-    expect(my_space.get_unavailable_dates).to eq [[21, 03, 2017]]
+    booking = Booking.create(date: "2017-03-22", space_id: 1)
+    expect(my_space.get_unavailable_dates).to eq [[22, 3, 2017]]
   end
 end
