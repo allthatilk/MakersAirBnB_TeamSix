c788ec15b0a1a8015cf2533e7aa188e28d469a6e
Flora Harvey <fharvey555@gmail.com>
2017-03-23 12:30:01 +0000

    Fix bug in booking requests

--- spec/features/availability_spec.rb
+++ spec/features/availability_spec.rb
@@ -6,8 +6,6 @@ feature 'space availability' do
 
   scenario 'seeing a space\'s availability tonight' do
     sign_in()
-    visit "/"
-    sign_in
     create_joy_room_listing
     click_link "The Joy Room"
     expect(page).to have_content("Available Tonight!")
@@ -22,17 +20,14 @@ feature 'filtering' do
 
   scenario 'filter the spaces list by availability' do
     sign_in()
-    p "pay attention"
     visit "/"
     create_joy_room_listing
     create_jims_room_listing
     Booking.create(date: Date.today, space_id: 2)
     fill_in("date", with: Date.today.strftime("%Y-%m-%d"))
-    p Booking.get(1)
     click_button "See Available Spaces"
     expect(page).to have_content("The Joy Room")
     expect(page).to_not have_content("Jim's Room")
-    p "look away"
   end
 
 end
