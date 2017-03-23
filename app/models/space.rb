95f87f6f5182c58e8f274268fad6bd769bb5acdd
Flora Harvey <fharvey555@gmail.com>
2017-03-23 18:33:57 +0000

    Spaces now remain available until booking request is approved

--- app/models/space.rb
+++ app/models/space.rb
@@ -16,13 +16,13 @@ class Space
   end
 
   def available_on_date?(date)
-    Booking.all(space_id: id, date: date).empty?
+    Booking.all(space_id: id, date: date, is_approved: true).empty?
   end
 
   def get_unavailable_dates
-    all_bookings = Booking.all(space_id: id)
+    all_approved_bookings = Booking.all(space_id: id, is_approved: true)
     unavailable_dates = []
-    all_bookings.each do |booking|
+    all_approved_bookings.each do |booking|
       unavailable_date = [booking.date.strftime('%d').to_i, booking.date.strftime('%m').to_i, booking.date.strftime('%Y').to_i]
       unavailable_dates << unavailable_date
     end
