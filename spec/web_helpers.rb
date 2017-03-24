caf689470f117acc1bd236647aaaae50ca64619b
Natalia Stafeeva <n.stafeeva@gmail.com>
2017-03-24 11:56:35 +0000

    Add listing page

--- spec/web_helpers.rb
+++ spec/web_helpers.rb
@@ -13,6 +13,7 @@ def sign_in(email = 'test@test.com', password = 'password')
 end
 
 def create_joy_room_listing
+  visit '/new-listing'
   fill_in("name", with: "The Joy Room")
   fill_in("description", with: "The happiest room of all.")
   fill_in("price", with: "10")
@@ -21,6 +22,7 @@ def create_joy_room_listing
 end
 
 def create_jims_room_listing
+  visit '/new-listing'
   fill_in("name", with: "Jim's Room")
   fill_in("description", with: "The saddest room of all.")
   fill_in("price", with: "100")
