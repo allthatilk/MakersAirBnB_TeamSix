c541db15f118be67cdc9264c94e688fd6f09fada
Alice Armstrong <tioma001@hotmail.com>
2017-03-24 16:50:25 +0000

    Replaced home button with logo

--- spec/features/profile_picture_spec.rb
+++ spec/features/profile_picture_spec.rb
@@ -14,5 +14,5 @@ feature "profile pictures" do
     visit '/'
     expect(page).to have_xpath("//img[contains(@src, 'https://s-media-cache-ak0.pinimg.com/originals/12/de/a5/12dea53a49fcc68dfc64cfb6f0de9df8.jpg')]")
   end
-  
+
 end
